e3f7a94ae9585dcfb9eceb9125d7af4286e93251
shreyasomapur02 <shreyasomapur1@gmail.com>
2026-06-01 09:49:20 +0530

    Create script.js

---
+++ demo/script.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>My Web Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>My Website</h1>
+        <button onclick="showMessage()">Click Me</button>
+        <p id="message"></p>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
